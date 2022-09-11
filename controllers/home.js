4c0e2b7ebaceb37b9ac1eccdee8adb9bd012c964
Glitch (glow-oxidized-oxygen) <none>
2022-09-11 17:17:46 +0000

    made route to home page for implementation of member signup

---
+++ controllers/home.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const logger = require("../utils/logger");
+
+const home = {
+  index(request, response) {
+    logger.info("home rendering");
+    const viewData = {
+      title: "Welcome to Weathertop Station",
+    };
+    response.render("home", viewData);
+  }
+};
+
+module.exports = home;
